a825e9f70dfe81754d923dfb8025dcb51c2b0ef5
zhangjunzhe <1315916174@qq.com>
2023-07-03 15:41:27 +0800

    安装了husky

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,26 +1,24 @@
 <script setup lang="ts">
-
+const str = '我爱你祖国'
 </script>
 
 <template>
   <div>
-  我是APP根组件
+    我是APP根组件
+    <div class="box">
+      <h1>{{ str }}</h1>
+    </div>
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: #000;
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  h1 {
+    color: #fff;
+  }
 }
 </style>
